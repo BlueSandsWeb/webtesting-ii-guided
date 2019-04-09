1a533f2f74b4fc5c042674982d19427fc24a8aa6
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-09 09:37:06 -0700

    created first frontend test!

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// step 1 - import library
+import { render } from 'react-testing-library';
+
 import App from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+//step 2
+it('renders successfully', () => {
+  render(<App></App>);
+})
