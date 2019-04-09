78f48a4fd44c7b07d1a3f2dd4ec758ff00af74ce
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-09 10:17:47 -0700

    added a hello world test.  One that is case sensative and one that is case insensative

--- src/App.test.js
+++ src/App.test.js
@@ -6,14 +6,27 @@ import { render } from 'react-testing-library';
 
 import App from './App';
 
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  //step 2 write a test
+  it('renders successfully', () => {
+    render(<App></App>);  // this returns functions that can help find things in the dom
+  })
 
-//step 2
-it('renders successfully', () => {
-  render(<App></App>);
+  it('renders Hello World', () => {
+    //this shows all of the functions that will help grab things on the dom
+    // const helpers = render(<App />)
+    // console.log('helpers', helpers);
+    const { getByText, queryByText, debug } = render(<App />);
+    debug(); // this renders the html to the terminal that jest is running in even if it passes
+    // getByText('hello world');     // this is case sensative
+    getByText(/hello world/i);     // this is case insensative
+  })
+
 })
+
