69b149eaa675c9db1abcf8d528094e32778f281e
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-09 10:37:42 -0700

    added tests for click events

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    message: 'No greeting for you',
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello World</h1>
+        <button onClick={this.greet}>Greet</button>
+        <button onClick={this.bye}>Bye</button>
+        <div>{this.state.message}</div>
       </div>
     );
+  }
+
+  greet = () => {
+    this.setState({ message: 'Hello Web 17' });
+  }
+
+  bye = () => {
+    this.setState({ message: 'Bye Web 17' });
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // step 1 - import library
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 
 import App from './App';
 
@@ -23,9 +23,27 @@ describe('<App />', () => {
     // const helpers = render(<App />)
     // console.log('helpers', helpers);
     const { getByText, queryByText, debug } = render(<App />);
-    debug(); // this renders the html to the terminal that jest is running in even if it passes
+    // debug(); // this renders the html to the terminal that jest is running in even if it passes
     // getByText('hello world');     // this is case sensative
     getByText(/hello world/i);     // this is case insensative
+    // expect(queryByText(/hello world/i)).not.toBeNull(); // won't break your test if it fails.
+  })
+
+  it('greets the team', () => {
+    const { getByText } = render(<App />);
+    const button = getByText(/greet/i);
+
+    fireEvent.click(button);
+    getByText(/hello web 17/i);
+
+  })
+
+  it('Says goodbye to the team', () => {
+    const { getByText } = render(<App />);
+    const button = getByText(/bye/i);
+
+    fireEvent.click(button);
+    getByText(/bye web 17/i);
   })
 
 })
